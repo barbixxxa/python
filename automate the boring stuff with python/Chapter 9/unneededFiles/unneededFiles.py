1e1aea62c51668437491d4446eb9e8f420cea4b7
theeam <eduuardomuller@gmail.com>
2019-06-17 17:07:00 -0300

    removing import

--- automate the boring stuff with python/Chapter 9/unneededFiles/unneededFiles.py
+++ automate the boring stuff with python/Chapter 9/unneededFiles/unneededFiles.py
@@ -1,7 +1,6 @@
 #! python3
 # unneededFiles.py - Write a program that walks through a folder tree and searches for exceptionally large files or folders—say, ones that have a file size of more than 100MB. Print these files with their absolute path to the screen.
 
-import shutil
 import os
 
 # input('Insert the folder path: ')
